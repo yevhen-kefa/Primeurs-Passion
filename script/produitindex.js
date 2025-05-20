47ffb60973a4975c613296284a58bf2adedd7340
Yevhen Kefa <121124942+yevhen-kefa@users.noreply.github.com>
2025-05-20 17:02:28 +0200

    Delete script directory

--- script/produitindex.js
+++
@@ -1,5 +0,0 @@
-function scrollHorizontally(event) {
-    event.preventDefault();
-    const delta = Math.max(-1, Math.min(1, event.wheelDelta || -event.detail));
-    document.querySelector(".horizontal-scroll").scrollLeft -= delta * 40; // Ajustez la vitesse du défilement ici
-}
